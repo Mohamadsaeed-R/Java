8094c6017d5a3df7cac6e19bf7c7c292ee954ed3
Mohamadsaeed-R <rezae.mohamadsaeed@gmail.com>
2024-07-21 18:03:12 +0330

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Project1 {
+        public static void main(String[]args){
+            System.out.println("please enter an integer:\n");
+            Scanner input =new Scanner(System.in);{
+                int n =input.nextInt();
+
+                if (n==6) {
+                    System.out.println("you have entered 6");
+                }
+                else{
+                    System.out.println("you have not entered 6");
+
+                }
+            }
+        }
+}
