6bee2ffc25dc83cfca5f3a625cfe5c873e4d7c52
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-08-02 18:22:38 +0330

    Project5

--- Project5.java
+++ Project5.java
@@ -3,10 +3,16 @@ public class Project5 {
     public static void main(String[]args){
         Scanner input=new Scanner (System.in);
         float x;
-        System.out.println("Enter  a number:");
-        x=input.nextFloat();
-        System.out.format("%f\t%f\t%f",x,Math.pow(x,2),Math.pow(x,4));
-
+        String ans;
+        while(true){
+             System.out.println("Enter  a number:");
+            x=input.nextFloat();
+            System.out.format("%f\t%f\t%f",x,Math.pow(x,2),Math.pow(x,3));
+            System.out.println("do you want to continue?(y/n)");
+            ans=input.next();
+            ans=ans.toLowerCase();
+            if(ans.startsWith("y")) break;
+        }
 
     }
 }
