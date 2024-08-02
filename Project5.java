b55d58dea327a855dabd2f32e231f6f84a39eeaf
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-08-02 18:02:02 +0330

    Project5

--- Project5.java
+++ Project5.java
@@ -2,6 +2,11 @@ import java .util.*;
 public class Project5 {
     public static void main(String[]args){
         Scanner input=new Scanner (System.in);
+        float x;
+        System.out.println("Enter  a number:");
+        x=input.nextFloat();
+        System.out.format("%f\t%f\t%f",x,Math.pow(x,2),Math.pow(x,3));
+
 
     }
 }
