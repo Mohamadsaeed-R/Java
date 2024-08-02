171cbeae4f050791e578d64d6ee32aa5116e662c
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-08-02 18:25:15 +0330

    Project5

--- Project5.java
+++ Project5.java
@@ -5,7 +5,7 @@ public class Project5 {
         float x;
         String ans;
         while(true){
-             System.out.println("Enter  a number:");
+            System.out.println("Enter  a number:");
             x=input.nextFloat();
             System.out.format("%f\t%f\t%f\n",x,Math.pow(x,2),Math.pow(x,3));
             System.out.println("do you want to continue?(y/n)");
