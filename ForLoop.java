2d122d25952049cbcb2674c657efb7355e851e87
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-08-04 18:38:08 +0330

    My First For Loop

--- ForLoop.java
+++ ForLoop.java
@@ -1,7 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
         for (int i =0;i<10;i++){
-            System.out.format("%d\t%d\n",i,i*i);//calculates second exponent of the numbers
+            System.out.format("%d\t%d\n",i,i*i); //calculates second exponent of the numbers
         }
     }
 }
