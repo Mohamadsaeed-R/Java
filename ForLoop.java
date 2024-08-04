8ff68dc2229ceea744096b6238548151e5f5338b
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-08-04 18:35:45 +0330

    Project6

--- ForLoop.java
+++ ForLoop.java
@@ -1,4 +1,4 @@
-public class Project6 {
+public class ForLoop {
     public static void main(String[] args) {
         for (int i =0;i<10;i++){
             System.out.format("%d\t%d\n",i,i*i);//calculates second exponent of the numbers
